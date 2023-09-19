2b8f1a0a3efe9d654c5dd807a84ae7da8b1c01a0
Bailey Long <bailey-long@live.nmit.ac.nz>
2023-09-20 09:38:11 +1200

    index page setup

--- app.js
+++ app.js
@@ -38,4 +38,9 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
+// app listener
+app.listen(3000, function() {
+    console.log('Server running at http://localhost:3000');
+});
+
 module.exports = app;
