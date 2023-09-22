afd1e4c5ada84c702c1b9324cd503eb502ef6ad0
Rhylei Tremlett <Rhylei@aureate.dev>
2023-09-22 19:44:30 +1200

    created user model, get all user route and connect to mongodb middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
+//SETUP - Modules
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//SETUP - Import models
+const User = require('../models/user')
+
+
+/* -------------------------------------------------------------------------- */
+/*                          //SECTION - Get all users                         */
+/* -------------------------------------------------------------------------- */
+router.get('/user/all', async (req, res, next) => {
+    try {
+        // Retrieve all users from the database.
+        let users = await User.find()
+
+        // If users evaluates to true then there are users.
+        if (users) {
+            return res.status(200).json(users);
+        } else {
+            return res.status(404).json({ error: "Not found. No users exist."});
+        }
+    } catch (error) {
+        // If there's an error, respond with a server error.
+        return res.status(500).json({ error: "Something went wrong on our end. Please try again. "});
+    }
 });
 
 module.exports = router;
