c64922f0929b2b471e19980216af0a59651c11a7
Xander-Crocker <Xander-Crocker@live.nmit.ac.nz>
2023-09-25 12:15:59 +1300

    update method

--- routes/users.js
+++ routes/users.js
@@ -145,5 +145,43 @@ router.delete('/user/delete/:username', async (req, res, next) =>{
     }
 });
 
+/* -------------------------------------------------------------------------- */
+/*                         //SECTION - Update user                         */
+/* -------------------------------------------------------------------------- */
+
+router.put('/user/update/:id', async (req, res) => {
+    try {
+        const {
+            password
+        } = req.body;
+
+        // Hash and salt the password, then store in database.
+        await bcrypt.hash(password, saltRounds, async function(err, hash) {
+            if (err) {
+                console.log(err);
+            }
+
+            // update the users password to the database
+            User.updateOne({ _id: req.params.id }, 
+            { password: hash }).then(result => {
+                if (result.matchedCount = 0) {
+                    return res.status(404).send({
+                        message: 'Document not found.'
+                        });
+                } else if (result.modifiedCount = 0) {
+                    return res.status(500).send({ error: 'Document found, but could not be updated.' });
+                } else {
+                    return res.status(201).send({
+                    message: 'User password changed successfully.'
+                    });
+                }
+        });
+    });
+
+    } catch (error) {
+        // If there's an error, respond with an error message
+        return res.status(500).send({ error: 'Something went wrong. Please try again.' });
+    }
+});
 
 module.exports = router;
