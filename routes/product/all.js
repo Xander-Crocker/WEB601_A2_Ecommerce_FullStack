d1cf86ce8bc3ddd60a5f8dbfe865604b86b5d01f
Rhylei Tremlett <Rhylei@aureate.dev>
2023-10-02 20:49:07 +1300

    completed products route, and implementd front end ejs to display data

--- routes/product/all.js
+++ routes/product/all.js
@@ -1,8 +1,7 @@
 //SETUP - Modules
-var express = require('express');
+const express = require('express');
 const axios = require('axios').default;
-var router = express.Router();
-
+const router = express.Router();
 
 
 const axiosRequest = axios.create({
@@ -18,24 +17,26 @@ const axiosRequest = axios.create({
 router.get('/all', async (req, res, next) => {
     try {
         // Ensure there is a valid session and the user is properly authorised.
-        // if (req.session.user) {
-        //     if (req.session.user.role !== 'admin') {
-        //         return res.status(403).send({
-        //             message: 'You are not authorized to perform this action.',
-        //         });
-        //     }
-        // } else {
-        //     return res.status(401).send({
-        //         message: 'You are currently not authenticated.',
-        //     });
-        // }
+        const originIsTrusted = process.env.TRUSTED_ORIGINS.includes(req.hostname) || process.env.TRUSTED_ORIGINS.includes(req.ip);
+        if (!originIsTrusted) {
+            if (req.session.user) {
+                if (req.session.user.role !== 'admin') {
+                    return res.status(403).send({
+                        message: 'You are not authorized to perform this action.',
+                    });
+                }
+            } else {
+                return res.status(401).send({
+                    message: 'You are currently not authenticated.',
+                });
+            }
+        }
 
         // Retrieve all products from Printify.
         let products = await axiosRequest.get('/shops/11988834/products.json')
-
         // If users evaluates to true then there are users.
-        if (products) {
-            return res.status(200).json(products);
+        if (products.data) {
+            return res.status(200).json(products.data);
         } else {
             return res.status(404).json({ error: "No products founds."});
         }
