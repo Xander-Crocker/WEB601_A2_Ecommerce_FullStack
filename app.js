c58ac56ab25e8d68a155e5a4e4bf43208894234f
Xander-Crocker <Xander-Crocker@live.nmit.ac.nz>
2023-10-04 09:54:02 +1300

    login/register/logout

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 app.use('/', viewRouter);
 app.use('/api/user', userRouter);
---
+++ app/logout.js
@@ -0,0 +1,13 @@
+
+// Logout user (on click event)
+document.getElementById('logout').addEventListener('click', async() => {
+    // Send get request to logout endpoint
+    const response = await fetch('/api/user/logout', {
+        method: "GET",
+    });
+
+    if (response.status === 200 && response.ok === true) {
+        alert('Logout successful.')
+        window.location.replace('http://localhost:3000')
+    }
+})
